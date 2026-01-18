57bdb54934a0dccda3df66c76b18ef3cddd48b91
xciav <xcia@itracasa.es>
2026-01-18 08:43:21 +0100

    se añade camera.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:camera/camera.dart';
 import '../main.dart';
 import 'camera_screen.dart';
 import 'aruco_pdf_screen.dart';
